d050b3937996cc42b80c9ff74b3521326ce93ce2
ProfDian <129824306+ProfDian@users.noreply.github.com>
2023-04-04 13:37:20 +0700

    Update kelompok.py

--- kelompok.py
+++ kelompok.py
@@ -1,6 +1,8 @@
 class no_kelshift:
-
+    
     def kelshift(self, kel, shift):
+        
             self.kel = kel
             self.shift = shift
+            
             return print(f"\nAnggota Kelompok {self.kel} Shift {self.shift} :\n")
